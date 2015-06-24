4f24f85bb5c5e38f88f127978c57eca5da81afec
geraldanekwe <gerald.anekwe.ii@gmail.com>
2015-06-23 23:20:33 -0700

    remove if/else statements for calc

--- main.js
+++ main.js
@@ -3,20 +3,19 @@ var api = {
   http: require("http"),
   url: require('url'),
   md5: require("blueimp-md5").md5,
-  add: function(a, b) {
-    return a + b;
-  },
-  subtract: function(a, b) {
-    return a - b;
-  },
-  multiply: function(a, b) {
-    return a * b;
-  },
-  divide: function(a, b) {
-    return a / b;
-  },
-  isWord: function(word) {
-    return word.length > 0;
+  operation: {
+    "+": function(a, b) {
+      return a + b;
+    },
+    "-": function(a, b) {
+      return a - b;
+    },
+    "*": function(a, b) {
+      return a * b;
+    },
+    "/": function(a, b) {
+      return a / b;
+    }
   },
   counter: function(text) {
     var wordsMatch = text.match(/\w+/g) || {
@@ -52,28 +51,10 @@ function responseHandler(request, response) {
     var expressionArr = expression.match(/(^[\d]+)(\D)([\d]+)/);
     var var1 = parseInt(expressionArr[1]);
     var var2 = parseInt(expressionArr[3]);
-    if (expressionArr[2] === "+") {
-      var result = api.add(var1, var2);
-      response.write(expressionArr[0].toString() + " = ");
-      response.write(result.toString());
-      response.end();
-    } else if (expressionArr[2] === "-") {
-      var result = api.subtract(var1, var2);
-      response.write(expressionArr[0].toString() + " = ");
-      response.write(result.toString());
-      response.end();
-    } else if (expressionArr[2] === "*") {
-      var result = api.multiply(var1, var2);
-      response.write(expressionArr[0].toString() + " = ");
-      response.write(result.toString());
-      response.end();
-    } else if (expressionArr[2] === "/") {
-      var result = api.divide(var1, var2);
-      response.write(expressionArr[0].toString() + " = ");
-      response.write(result.toString());
-      response.end();
-    }
-
+    var result = api.operation[expressionArr[2]](expressionArr[1], expressionArr[3]);
+    console.log(result);
+    response.write(result.toString());
+    response.end();
   } else if (request.url.match("/Counts/")) {
     var sentence = request.url.replace(/[\/][A-z]+[\/]/g, '').replace(/%20/g, " ");
     var result = api.counter(sentence);
